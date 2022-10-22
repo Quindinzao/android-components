0a58f8048e9165203cf2326584e16ecf3909fe5c
Joao Fernandes <joao.fernandes@impar.com.br>
2022-10-22 14:54:14 -0300

    add checkbox

--- app/src/main/java/com/quindinzao/components/MainActivity.java
+++ app/src/main/java/com/quindinzao/components/MainActivity.java
@@ -3,6 +3,8 @@ package com.quindinzao.components;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckBox checkBox = findViewById(R.id.checkBox);
+
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                checkBox.setText(R.string.is_checked);
+            } else {
+                checkBox.setText(R.string.is_not_checked);
+            }
+        });
     }
 }
