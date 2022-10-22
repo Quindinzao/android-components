f89aa9a12b417874c96cf204b376e7c12f0c0bf9
Joao Fernandes <joao.fernandes@impar.com.br>
2022-10-22 13:43:24 -0300

    add switch

--- app/src/main/java/com/quindinzao/components/MainActivity.java
+++ app/src/main/java/com/quindinzao/components/MainActivity.java
@@ -3,6 +3,10 @@ package com.quindinzao.components;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwitchMaterial swHelloWorld = findViewById(R.id.swHelloWorld);
+        TextView txtHelloWorld = findViewById(R.id.txtHelloWorld);
+
+        swHelloWorld.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                txtHelloWorld.setText("ON");
+            } else {
+                txtHelloWorld.setText("OFF");
+            }
+        });
     }
 }
