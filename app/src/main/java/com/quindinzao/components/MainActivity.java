328975a26633d9b6f43a54d63bd3e54e590cd70d
Joao Fernandes <joao.fernandes@impar.com.br>
2022-10-27 00:38:28 -0300

    add radio button and radio group

--- app/src/main/java/com/quindinzao/components/MainActivity.java
+++ app/src/main/java/com/quindinzao/components/MainActivity.java
@@ -3,6 +3,8 @@ package com.quindinzao.components;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup rgOptions = findViewById(R.id.rgOptions);
+
+        rgOptions.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.optionA) {
+                Toast.makeText(this, getString(R.string.option_a) + " is selected!", Toast.LENGTH_SHORT).show();
+            } else if (checkedId == R.id.optionB) {
+                Toast.makeText(this, getString(R.string.option_b) + " is selected!", Toast.LENGTH_SHORT).show();
+            } else if (checkedId == R.id.optionC) {
+                Toast.makeText(this, getString(R.string.option_c) + " is selected!", Toast.LENGTH_SHORT).show();
+            } else {
+                return;
+            }
+        });
     }
 }
