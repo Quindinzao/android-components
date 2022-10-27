df9c728da168fee7267bb51d48b2e49afbaaa3bf
Joao Fernandes <joao.fernandes@impar.com.br>
2022-10-27 03:19:50 -0300

    add edit text

--- app/src/main/java/com/quindinzao/components/MainActivity.java
+++ app/src/main/java/com/quindinzao/components/MainActivity.java
@@ -3,6 +3,9 @@ package com.quindinzao.components;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edtUsername = findViewById(R.id.edtUsername);
+        Button btnShowUsername = findViewById(R.id.btnShowUsername);
+
+        btnShowUsername.setOnClickListener(v -> {
+            if (!edtUsername.getText().toString().equals("")) {
+                Toast.makeText(this, edtUsername.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
