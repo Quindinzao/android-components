6d00cf5ad1d0a9cfbb668985d16cd45b21e5dbc4
Joao Fernandes <joao.fernandes@impar.com.br>
2022-10-27 10:59:19 -0300

    add toggle button

--- app/src/main/java/com/quindinzao/components/MainActivity.java
+++ app/src/main/java/com/quindinzao/components/MainActivity.java
@@ -3,6 +3,9 @@ package com.quindinzao.components;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                Toast.makeText(this, getText(R.string.is_checked).toString(), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, getText(R.string.is_not_checked).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
